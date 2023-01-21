8a8bccbb17c51e72aa07d4b80ae2e3e9624d8dd7
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-01-21 22:48:07 +0000

    Sync LeetCode submission - Third Maximum Number (python3)

---
+++ my-folder/problems/third_maximum_number/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort()
+        i = len(nums) - 1
+        while i > 0:
+            if nums[i] == nums[i - 1]:
+                nums.pop(i)
+            i -= 1
+        ans = 0
+        if len(nums) < 3:
+            ans=max(nums)
+        if len(nums) >= 3:
+            max_1 = max(nums)
+            nums.remove(max_1)
+            max_2 = max(nums)
+            nums.remove(max_2)
+            max_3 = max(nums)
+            ans=max_3
+        return ans
