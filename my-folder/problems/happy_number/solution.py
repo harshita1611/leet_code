a92d7f34e03e6e0f3a59e95f96bc98cdc6be2ac5
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-01-29 13:22:11 +0000

    Sync LeetCode submission - Happy Number (python3)

---
+++ my-folder/problems/happy_number/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        hset=set()
+        while n!=1 :
+            n = sum([int(i) ** 2 for i in str(n)])
+
+            if n in hset:
+                return False
+            hset.add(n)
+
+        return True
