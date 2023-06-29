e47e3d48e62ed80a04b5962a40c3987f820a5a04
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-06-29 14:26:09 +0000

    [LeetCode Sync] Runtime - 35 ms (59.13%), Memory - 16.3 MB (99.66%)

---
+++ my-folder/1960-check-if-the-sentence-is-pangram/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        s = set()
+        for i in sentence :
+            s.add(i)
+        if len(s)==26 :
+            return True
+        else :
+            return False
