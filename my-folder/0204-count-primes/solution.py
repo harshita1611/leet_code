4d711ee2ff5b92a0093be2c2266d8482a83c2021
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-08-19 11:15:15 +0000

    [LeetCode Sync] Runtime - 1683 ms (56.18%), Memory - 55 MB (98.23%)

---
+++ my-folder/0204-count-primes/solution.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n<=2:
+            return 0
+        is_prime=[True]*n
+        is_prime[0]=is_prime[1]=False
+        for i in range(2,int(math.sqrt(n))+1):
+            if is_prime[i]==True:
+                for j in range(i*i , n , i):
+                    is_prime[j]=False
+        return sum(is_prime)
