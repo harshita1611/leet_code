e847b01fb23a32c3131d02be3f167fdbec650e40
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-08-21 17:18:44 +0000

    [LeetCode Sync] Runtime - 1326 ms (15.35%), Memory - 33.3 MB (18.19%)

---
+++ my-folder/0239-sliding-window-maximum/solution.py
@@ -0,0 +1,17 @@
+import collections 
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output=[]
+        queue= collections.deque()
+        l=r=0
+        while r < len(nums):
+            while queue and nums[queue[-1]]<nums[r] : 
+                queue.pop()
+            queue.append(r)
+            if l > queue[0]:
+                queue.popleft()
+            if (r+1)>=k :
+                output.append(nums[queue[0]])
+                l+=1
+            r+=1
+        return output
