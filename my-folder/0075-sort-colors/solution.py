6efba6c2ae1c18114342c027d672db0cba38bcd8
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-08-29 13:27:52 +0000

    [LeetCode Sync] Runtime - 41 ms (28.92%), Memory - 16.2 MB (99.97%)

--- my-folder/0075-sort-colors/solution.py
+++ my-folder/0075-sort-colors/solution.py
@@ -3,17 +3,11 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low=0
-        mid=0
-        high=len(nums)-1
-        while mid <= high :
-            if nums[mid] ==0 :
-                nums[low] , nums[mid] = nums[mid] , nums[low]
-                low+=1
-                mid+=1
-            elif nums[mid]==1 :
-                mid+=1
-            else : 
-                nums[high] , nums[mid] = nums[mid] , nums[high]
-                high-=1
+        n=len(nums)
+        for i in range(n):
+            min_idx = i
+            for j in range(i+1,n) :
+                if nums[j] < nums[min_idx] :
+                    min_idx=j
+            nums[i] , nums[min_idx] = nums[min_idx] ,nums[i]
 
