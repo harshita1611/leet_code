1bb3e64d05fa48af46992f6f8df3c017389856f6
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-03 14:19:39 +0000

    Sync LeetCode submission - Count Good Numbers (python3)

---
+++ my-folder/problems/count_good_numbers/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        mod=1000000007
+        def binpow(a,b):
+            res=1
+            while b :
+                if b & 1 :
+                    res=(res*a) %mod
+                a= (a*a) %mod
+                b>>=1
+            return res
+
+        odd=n//2
+        even=(n-odd)
+        odd_pow = binpow(4,odd)
+        even_pow = binpow(5,even)
+
+        return (odd_pow * even_pow)%mod
