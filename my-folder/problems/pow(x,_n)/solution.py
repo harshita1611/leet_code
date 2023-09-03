d18b77c4a083d934b1b48842f139e77e0300bdd3
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-03 11:26:19 +0000

    Sync LeetCode submission - Pow(x, n) (python3)

--- my-folder/problems/pow(x,_n)/solution.py
+++ my-folder/problems/pow(x,_n)/solution.py
@@ -1,3 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        if n==0 :
+            return 1
+        if n < 0 :
+            x=1/x
+            n= abs(n)
+            return self.myPow(x,n)
+        if n%2 ==0 :
+            return self.myPow(x**2,n//2)
+        else :
+            return x * self.myPow(x,n-1)
