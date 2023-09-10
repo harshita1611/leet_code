cc4d8d37887824685332cba72756a86f7dfccd05
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-10 10:12:41 +0000

    Sync LeetCode submission - Next Greater Element I (python3)

---
+++ my-folder/problems/next_greater_element_i/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res=[]
+        for i in nums1:
+            for j in range(nums2.index(i),len(nums2)):
+                if nums2[j]>i:
+                    res.append(nums2[j])
+                    break
+                elif j==len(nums2)-1:
+                        res.append(-1)
+                        
+            
+        return res
