342fa9c227ac6e42736dc7bbe070b5def731388a
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-14 19:49:42 +0000

    [LeetCode Sync] Runtime - 1563 ms (7.37%), Memory - 23.8 MB (86.46%)

--- my-folder/0948-sort-an-array/solution.py
+++ my-folder/0948-sort-an-array/solution.py
@@ -1,29 +1,26 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def mergesort(nums):
-            if len(nums)>1:
-                mid=len(nums)//2
-                left = nums[:mid]
-                right=nums[mid:]
-                mergesort(left)
-                mergesort(right)
-                i=j=k=0
-                while i <len(left) and j <len(right):
-                    if left[i] < right[j]:
-                        nums[k]=left[i]
-                        i+=1
-                    else:
-                        nums[k]=right[j]
-                        j+=1
-                    k+=1
-                while i < len(left):
-                    nums[k] = left[i]
-                    i += 1
-                    k += 1
-                while j < len(right):
-                    nums[k] = right[j]
-                    j += 1
-                    k += 1
-            return nums
-        return mergesort(nums)
-        
+      if len(nums) >1 :
+        mid=len(nums)//2
+        L=nums[:mid]
+        R=nums[mid:]
+        self.sortArray(L)
+        self.sortArray(R)
+        i=j=k=0
+        while i < len(L) and j < len(R) :
+          if L[i] <= R[j] :
+            nums[k]=L[i]
+            i+=1
+          else :
+            nums[k]=R[j]
+            j+=1
+          k+=1
+        while i<len(L) :
+          nums[k]=L[i]
+          i+=1
+          k+=1
+        while j < len(R) :
+          nums[k]=R[j]
+          j+=1
+          k+=1
+      return nums
