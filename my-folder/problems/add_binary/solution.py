2a11fd4efec6da5c8005b1c7aaa1ce37bd0c9364
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-15 13:44:46 +0000

    Sync LeetCode submission - Add Binary (python3)

--- my-folder/problems/add_binary/solution.py
+++ my-folder/problems/add_binary/solution.py
@@ -1,7 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = int(a, 2)
-        b = int(b, 2)
-        c = a + b
-        d = bin(c)
-        return d[2:]
+        return bin(int(a,2)+int(b,2))[2:]
