6f01a876824eaccff536829d72d415b65b1b20b6
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-25 17:31:46 +0000

    [LeetCode Sync] Runtime - 43 ms (25.70%), Memory - 16.4 MB (99.94%)

--- my-folder/0389-find-the-difference/solution.py
+++ my-folder/0389-find-the-difference/solution.py
@@ -1,9 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        t_set = set(t)
-        x=""
-        for i in t_set:
-            if s.count(i) != t.count(i) :
-                x+=i
-        return x
-
+        res=0
+        for i in s+t :
+            res^=ord(i)
+        return chr(res)
