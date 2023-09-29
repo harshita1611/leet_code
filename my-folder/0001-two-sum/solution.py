4a8163ed83b4d4b7e95d44d9da53e159d9a6ceb4
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-09-29 10:08:33 +0000

    [LeetCode Sync] Runtime - 70 ms (41.95%), Memory - 17.5 MB (60.46%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range (len(nums)) :
-            for j in range (i+1 , len(nums)) :
-                if target - nums[i] == nums[j] :
-                    return [i,j]
-            
-        return None
+        hashmap={}
+        for i in range(len(nums)):
+            curr = target - nums[i]
+            if curr in hashmap.keys() :
+                return [hashmap[curr],i]
+            else:
+                hashmap[nums[i]]=i
