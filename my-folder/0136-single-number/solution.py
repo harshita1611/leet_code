f06d355bc416af8ad6c7bfd882190bf2cbf59819
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-10-04 14:47:23 +0000

    [LeetCode Sync] Runtime - 121 ms (22.86%), Memory - 18.9 MB (98.67%)

--- my-folder/0136-single-number/solution.py
+++ my-folder/0136-single-number/solution.py
@@ -1,13 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        freq={}
-        ans=0
+        res=0
         for i in nums :
-            if i in freq :
-                freq[i]+=1
-            else :
-                freq[i]=1
-        for j in freq :
-            if freq[j]==1 : 
-                ans=j
-        return ans
+            res^=i
+        return res
