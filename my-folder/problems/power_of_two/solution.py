ac9b20e1a7fe18962680c066a268c92ac91096c3
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-10-04 13:32:04 +0000

    Sync LeetCode submission - Power of Two (python3)

--- my-folder/problems/power_of_two/solution.py
+++ my-folder/problems/power_of_two/solution.py
@@ -2,8 +2,7 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n==0 :
             return False
-        while n!=1 :
-            if n%2!=0:
-                return False
-            n=n//2
-        return True
+        if n& (n-1)== 0 :
+            return True
+        else:
+            return False
