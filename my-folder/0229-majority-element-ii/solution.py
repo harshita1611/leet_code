32c5ed7b881d0476b7dde08a52fdcf3fcfeaa9ca
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-10-05 06:15:24 +0000

    [LeetCode Sync] Runtime - 116 ms (7.27%), Memory - 17.7 MB (95.72%)

--- my-folder/0229-majority-element-ii/solution.py
+++ my-folder/0229-majority-element-ii/solution.py
@@ -1,14 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        freq = {}
-        for i in nums:
-            if i in freq :
-                freq[i] += 1
-            else :
-                freq[i]=1
-        result = []
-        for i in freq:
-            if freq[i] > (n // 3):
-                result.append(i)
-        return result
+        dict={}
+        for i in range(len(nums)):
+            try:
+                dict[nums[i]]+=1
+            except:
+                dict[nums[i]]=1
+        key=list(dict.keys())
+        value=list(dict.values())
+        res=[]
+        for i in range(len(value)):
+            if value[i]>len(nums)//3:
+                res.append(key[i])
+        return res
