621f41b750034fc12ecc77e8fb576458571381d2
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-11-25 17:32:08 +0000

    Sync LeetCode submission - Next Greater Element I (python3)

--- my-folder/problems/next_greater_element_i/solution.py
+++ my-folder/problems/next_greater_element_i/solution.py
@@ -1,16 +1,13 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ans= {}
         stack=[]
-        res=[-1]*len(nums2)
+        
         for i in range(len(nums2)):
-            while stack and nums2[i]>nums2[stack[-1]]:
-                index=stack.pop()
-                res[index]=nums2[i]
-            stack.append(i)
-
-        ans=[]
+            while stack and nums2[i]>stack[-1]:
+                ans[stack.pop()]=nums2[i]
+            stack.append(nums2[i])
+            
         for i in range(len(nums1)):
-            index_ki_value_num2_mein=nums2.index(nums1[i])
-            ans.append(res[index_ki_value_num2_mein])
-
-        return ans
+            nums1[i]=ans.get(nums1[i],-1)
+        return nums1
