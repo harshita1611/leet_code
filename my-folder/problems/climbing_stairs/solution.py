4a9a9b8af8588a17321441d81a7c4174278ebda0
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-12-24 13:30:59 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -1,12 +1,19 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n==1 :
-            return 1
-        if n==2 :
-            return 2
-        dp=[0]*(n+1)
-        dp[0]=1
-        dp[1]=2
-        for i in range(2,n) :
-            dp[i]=dp[i-1] + dp[i-2]
-        return dp[n-1]
+        def memoisation(n,dp):
+            if n == 0 or  n == 1:
+                return 1
+            if dp[n]!=-1:
+                return dp
+            
+            return (memoisation(n-1,dp)+memoisation(n-2,dp))
+        
+
+        def tabulation(n,dp):
+            dp[0]=1
+            dp[1]=1
+            for i in range(2,n+1):
+                dp[i]=dp[i-1]+dp[i-2]
+            return dp[n]
+        dp=[-1]*(n+1)
+        return tabulation(n,dp)
