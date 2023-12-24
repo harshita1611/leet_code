0c4be432081fdbd01fbffe705a41c16e292cc721
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-12-24 19:05:15 +0000

    [LeetCode Sync] Runtime - 37 ms (49.54%), Memory - 17.2 MB (12.42%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,15 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n=len(nums)
-        if n==1 :
-            return nums[0]
-        if n==2 :
-            return max(nums[0],nums[1])
-        dp=[0]*(n+1)
-        dp[0]=nums[0]
-        dp[1] = nums[1]
-        
-        dp[2] = max(nums[1], nums[2]+nums[0])
-        for i in range(3,n) :
-            dp[i] = max(dp[i-2] + nums[i] , dp[i-3] +nums[i])
-        return max(dp[n-1], dp[n-2]) 
+        # def memoisation(index,nums,dp):
+        #     if index==0:
+        #         return nums[0]
+        #     if index<0:
+        #         return 0
+        #     if dp[index]!=-1:
+        #         return dp[index]
+
+        #     pick = nums[index]+memoisation(index-2,nums,dp)
+        #     notpick=0+memoisation(index-1,nums,dp)
+
+        #     dp[index]=max(pick,notpick)
+
+        #     return dp[index]
+        # dp=[-1]*(len(nums)+1)
+        # return memoisation(len(nums)-1,nums,dp)
+
+        def tabulation(n, nums, dp):
+            dp[0] = nums[0]
+            for i in range(1,len(nums)):
+                pick = nums[i]
+                if i>1:
+                    pick+=dp[i-2]
+                notpick = 0+dp[i-1]
+                dp[i] = max(pick, notpick)
+
+            return dp[len(nums)-1]
+
+        dp = [-1]*(len(nums)+1)
+        return tabulation(len(nums)-1, nums, dp)
