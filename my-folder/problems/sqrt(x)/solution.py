4161889a0494e85585662ef30294696cfa56b2c5
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2023-12-29 18:10:01 +0000

    Sync LeetCode submission - Sqrt(x) (python3)

--- my-folder/problems/sqrt(x)/solution.py
+++ my-folder/problems/sqrt(x)/solution.py
@@ -1,4 +1,47 @@
-import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(math.sqrt(x))
+        def dquare_root(x):
+            low=0
+            high=x
+            ans=x
+            if (x == 0):
+                return 0
+            if (x == 1):
+                return 1
+            while low<= high : 
+                mid=(low+high)//2
+                
+                if (mid*mid)>x:
+                    ans=mid
+                    high=mid-1
+                else:
+                    low=mid+1
+            return ans-1
+
+        return  dquare_root(x)
+
+
+# class Solution {
+# public:
+
+#     int mySqrt(int x) {
+#         int start=0;
+#         int end=x;
+#         long long ans=0,sqaure=0,mid;
+#         if(x==0) return 0;
+#         while(start<=end){
+#             mid=(start+end)/2;
+#             sqaure=(mid * mid );
+#             if(sqaure<=x){
+#                 ans=mid;
+#                 start=mid+1;
+#             }else if(x>sqaure){
+#                 start=mid+1;
+#             }else{
+#                 end=mid-1;
+#             }
+#         }
+#         return ans;
+
+#     }
+# }; 
