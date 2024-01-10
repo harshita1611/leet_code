6096c87b3688e84fca0f76a4b3d288f27ed13bdb
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-01-10 11:09:58 +0000

    Sync LeetCode submission - Rotate String (python3)

--- my-folder/problems/rotate_string/solution.py
+++ my-folder/problems/rotate_string/solution.py
@@ -1,13 +1,3 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        s = list(s)
-        i=0
-        while i < len(s) :
-            if "".join(s)==goal :
-                return True
-            s.append(s[0])
-            s.pop(0)
-
-            i+=1
-            
-        return False
+        return goal in (s+s) and (len(s)==len(goal))
