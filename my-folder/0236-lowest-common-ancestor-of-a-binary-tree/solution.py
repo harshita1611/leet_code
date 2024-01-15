4a60968ba0995659b51a0a1d8b0657e2878db34b
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-01-15 18:16:29 +0000

    [LeetCode Sync] Runtime - 51 ms (52.22%), Memory - 22 MB (5.37%)

--- my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
+++ my-folder/0236-lowest-common-ancestor-of-a-binary-tree/solution.py
@@ -7,29 +7,14 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def find_path(curr,target,path):
-            if curr is None:
-                return False
-            path.append(curr.val)
-            if curr.val==target.val:
-                return True
+        if root == None or root==p or root==q:
+            return root
+        left=self.lowestCommonAncestor(root.left,p,q)
+        right=self.lowestCommonAncestor(root.right,p,q)
 
-            left_subtree=find_path(curr.left,target,path)
-            if left_subtree:
-                return True
-            right_subtree=find_path(curr.right,target,path)
-            if right_subtree:
-                return True
-
-            path.pop()
-            return False
-        path_1=[]
-        path_2=[]
-        find_path(root,p,path_1)
-        find_path(root,q,path_2)
-        i=0 
-        while i<len(path_1) and i<len(path_2) :
-            if path_1[i]!=path_2[i]:
-                break
-            i+=1
-        return TreeNode(path_1[i-1])
+        if left==None:
+            return right
+        elif right==None:
+            return left
+        else:
+            return root
