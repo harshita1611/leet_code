668ff6d96ed163a40d1c659ccd99024c5670d1a3
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-03-26 06:10:50 +0000

    [LeetCode Sync] Runtime - 295 ms (67.07%), Memory - 35.1 MB (17.61%)

--- my-folder/0041-first-missing-positive/solution.py
+++ my-folder/0041-first-missing-positive/solution.py
@@ -1,8 +1,10 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         count=1
+        n=len(nums)
         nums.sort()
-        for i in range(len(nums)):
+        ans=set(nums)
+        for i in range(n):
             if nums[i]==count:
                 count+=1
         return count
