e6ef863afa600de1fa49fe02ccae53529b2101e7
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-05-01 07:45:08 +0000

    [LeetCode Sync] Runtime - 416 ms (6.04%), Memory - 17.4 MB (15.56%)

--- my-folder/0102-binary-tree-level-order-traversal/solution.py
+++ my-folder/0102-binary-tree-level-order-traversal/solution.py
@@ -5,32 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def height(self,root):
+        if not root:
+            return 0
+        return max(self.height(root.left),self.height(root.right)) +1
+    def currLevel(self,root,level):
+        if root is None:
+            return []
+        if level==1 :
+            return [root.val]
+        if level>1:
+            left=self.currLevel(root.left,level-1)
+            right=self.currLevel(root.right,level-1)
+            return left + right
+
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        def height(root):
-            if root is None :
-                return 0
-            left_height= height(root.left)
-            right_height=height(root.right)
-
-            if left_height > right_height :
-                return left_height + 1
-            else :
-                return right_height +1 
-
-        def currentlevel(root,level) :
-            if root is None :
-                return [] 
-            if level==1 :
-                return [root.val]
-            elif level > 1 :
-                left_values=currentlevel(root.left , level-1)
-                right_values=currentlevel(root.right , level-1)
-                return left_values + right_values
-        
-        arr = []
-        h = height(root)
-        for i in range(1, h + 1):
-            level_values = currentlevel(root, i)
-            if level_values:
-                arr.append(level_values)
-        return arr
+        height=self.height(root)
+        ans=[]
+        for i in range(1,height+1):
+            ans.append(self.currLevel(root,i))
+        return ans
