78235f8e1cdb38f9c3946885246f27e9e47591b6
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-05-29 08:59:29 +0000

    [LeetCode Sync] Runtime - 47 ms (26.58%), Memory - 16.9 MB (23.42%)

--- my-folder/0205-isomorphic-strings/solution.py
+++ my-folder/0205-isomorphic-strings/solution.py
@@ -1,3 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        return len(set(s))==len(set(t))==len(set(zip(s,t)))
+        h1={}
+        h2={}
+        for i in range(len(s)):
+            if not h1.get(s[i]):
+                h1[s[i]]=t[i]
+            if not h2.get(t[i]):
+                h2[t[i]]=s[i]
+            
+            if h1[s[i]]!=t[i] or h2[t[i]]!=s[i]:
+                return 0
+        return 1
