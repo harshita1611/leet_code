0ef57c1f4aa96b3e34f8de26b1afc41d3fa1a01e
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-05-29 07:12:05 +0000

    [LeetCode Sync] Runtime - 36 ms (54.68%), Memory - 16.5 MB (86.08%)

--- my-folder/0198-house-robber/solution.py
+++ my-folder/0198-house-robber/solution.py
@@ -1,22 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # def memoisation(index,nums,dp):
-        #     if index==0:
-        #         return nums[0]
-        #     if index<0:
-        #         return 0
-        #     if dp[index]!=-1:
-        #         return dp[index]
-
-        #     pick = nums[index]+memoisation(index-2,nums,dp)
-        #     notpick=0+memoisation(index-1,nums,dp)
-
-        #     dp[index]=max(pick,notpick)
-
-        #     return dp[index]
-        # dp=[-1]*(len(nums)+1)
-        # return memoisation(len(nums)-1,nums,dp)
-
         def tabulation(n, nums, dp):
             dp[0] = nums[0]
             for i in range(1,len(nums)):
@@ -28,5 +11,5 @@ class Solution:
 
             return dp[len(nums)-1]
 
-        dp = [-1]*(len(nums)+1)
+        dp = [0]*(len(nums)+1)
         return tabulation(len(nums)-1, nums, dp)
