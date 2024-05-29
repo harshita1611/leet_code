b9acf9acc67a8a90b7d82b507241420106a0e797
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-05-29 11:41:01 +0000

    [LeetCode Sync] Runtime - 149 ms (89.47%), Memory - 27.9 MB (64.68%)

--- my-folder/0189-rotate-array/solution.py
+++ my-folder/0189-rotate-array/solution.py
@@ -3,14 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k=k%len(nums)
-        def reverse( arr, start , end) : 
-            while start < end :
-                arr[start] , arr[end] = arr[end], arr[start]
+        n=len(nums)
+        k=k%n
+        
+        def reverse(nums,start,end):
+            while start<end:
+                nums[start],nums[end]=nums[end],nums[start]
                 start+=1
                 end-=1
-        reverse(nums , 0 , len(nums)-1)
-        reverse(nums , 0, k-1)
-        reverse(nums, k ,len(nums)-1)
         
-        
+        reverse(nums,0,n-1)
+        reverse(nums,0,k-1)
+        reverse(nums,k,n-1)
+
