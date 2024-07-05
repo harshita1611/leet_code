9f85c7522e5538e3a04e871429504ff1de29264f
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-07-05 17:14:37 +0000

    [LeetCode Sync] Runtime - 515 ms (50.32%), Memory - 66.6 MB (9.74%)

---
+++ my-folder/0584-find-customer-referee/solution.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+    df=customer[(customer['referee_id']!=2)| (customer['referee_id'].isna())][['name']]
+    return df
