19aac852d8535cbc94838f211955b2493e242f83
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-07-05 17:18:34 +0000

    [LeetCode Sync] Runtime - 428 ms (84.14%), Memory - 0B (100.00%)

--- my-folder/0584-find-customer-referee/solution.sql
+++ my-folder/0584-find-customer-referee/solution.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-select name from Customer where referee_id != 2 or referee_id is null;
+select name from customer where referee_id != 2 OR referee_id IS NULL;      
+
