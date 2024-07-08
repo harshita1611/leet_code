f1846e2c40559c8b62a181f674e9baed6c5b5f3f
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-07-08 14:56:34 +0000

    [LeetCode Sync] Runtime - 43 ms (24.72%), Memory - 16.5 MB (86.85%)

--- my-folder/0008-string-to-integer-atoi/solution.py
+++ my-folder/0008-string-to-integer-atoi/solution.py
@@ -1,19 +1,20 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         s=s.strip()
-        if len(s)== 0 :
-            return 0 
-        i = 0 
+        if len(s)==0:
+            return 0
         sign=1
-        if s[0]=='-':
+        i=0
+        if s[0]=="-":
             sign=-1
             i+=1
-        elif s[0]=='+':
-            i+=1
-
-        res=0
-        while i<len(s) and s[i].isdigit() :
-            res= res*10 + int(s[i])
+        elif s[0]=="+":
             i+=1
         
-        return max(-2**31 ,min(sign*res,2**31-1))
+
+        ans=0
+        while i<len(s) and s[i].isdigit():
+            ans=ans*10 +int(s[i])
+            i+=1
+        
+        return max(-2**31,min(2**31-1,ans*sign))
