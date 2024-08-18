4a0c3f85b6eb5c670d7daaa55c5fb52d8ef004d6
harshita1611 <97826462+harshita1611@users.noreply.github.com>
2024-08-18 21:06:44 +0000

    [LeetCode Sync] Runtime - 97 ms (27.93%), Memory - 22.4 MB (9.92%)

--- my-folder/0049-group-anagrams/solution.py
+++ my-folder/0049-group-anagrams/solution.py
@@ -1,9 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res= collections.defaultdict(list)
-        for i in strs : 
-            count = [0]*26
-            for c in i : 
-                count[ord(c)-ord("a")]+=1
-            res[tuple(count)].append(i)
-        return res.values()
+        hashmap=defaultdict(list)
+        for s in strs:
+            count=[0]*26
+            for c in s:
+                count[ord(c)-ord('a')]+=1
+            hashmap[tuple(count)].append(s)
+        return hashmap.values()
+
